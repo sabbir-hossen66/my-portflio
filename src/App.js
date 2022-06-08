97f488a244f5072ecca9b060a9102b10e9e5fe19
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-06-09 03:17:55 +0600

    service section done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from './About';
 import './App.css';
 import Home from './Home';
+import Service from './Service';
 function App() {
   return (
     <div className='App'>
      <Home/>
      <About/>
+     <Service/>
     </div>
   );
 }
